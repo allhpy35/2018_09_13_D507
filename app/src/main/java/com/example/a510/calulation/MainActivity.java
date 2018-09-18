10a2fc4ea533dacc295cc599ca2a9fa104976959
HwnagInWoo <allhpy35@gmail.com>
2018-09-18 16:32:49 +0900

    Calculate_1

--- app/src/main/java/com/example/a510/calulation/MainActivity.java
+++ app/src/main/java/com/example/a510/calulation/MainActivity.java
@@ -5,9 +5,35 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    protected double ans = 0.; //Answer
+
+    protected void inputNum(){
+
+    }
+
+    double op4(double x){
+        return 0;
+    }
+
+    protected double mathOp(double x){
+        return 0;
+    }
+
+
+    void printNum(double x){
+
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+
+
+
+
+
 }
