b22a4c98bb8f68d2b34758766b45c1ca7015ad61
HwnagInWoo <allhpy35@gmail.com>
2018-09-18 16:49:44 +0900

    Calculate_2

--- app/src/main/java/com/example/a510/calulation/MainActivity.java
+++ app/src/main/java/com/example/a510/calulation/MainActivity.java
@@ -2,32 +2,50 @@ package com.example.a510.calulation;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     protected double ans = 0.; //Answer
+    protected Button btnEqual;
 
     protected void inputNum(){
 
     }
 
-    double op4(double x){
+  protected   double op4(String  sOp, double x){
         return 0;
     }
 
-    protected double mathOp(double x){
+    protected double mathOp(String sFun, double x){
         return 0;
     }
 
 
-    void printNum(double x){
+   protected void printNum(double x){
 
     }
+
+    protected void calculate(){
+
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        btnEqual = (Button)findViewById(R.id.btnEqual);
+
+        btnEqual.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                calculate();
+            }
+        });
     }
 
 
